d7def49145422553e45c7efd46313175cd1b2fce
Yongjia Sun <thomastank0926@gmail.com>
2022-11-19 05:03:15 +0000

    add user authentication feature

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,8 @@
 class AboutController < ApplicationController
+
+  # Add a before_action to any controller that you want to secure. 
+  # This will force user's to login before they can see the actions in this controller.
+  # before_action :authorize
   def index
     @description = "Looking for a way to add some life to your home? Look no further than our online plant shop! We have a wide variety of beautiful plants and flowers, as well as all the accessories you need to keep them healthy and happy. Plus, our prices are unbeatable! So what are you waiting for?
 
